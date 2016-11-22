f1f414cf7ebc8a121af9d65761ea0048ae793f05
Batiazinga <luc.nguyenthe@gmail.com>
2016-11-22 22:24:54 +0100

    Test decomposition in main

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "log"
+import "fmt"
+
+var inputs = []struct{ base, n int }{
+	{2, 5},
+	{2, 10},
+	{2, 1023},
+	{2, 1024},
+	{3, 5},
+	{3, 10},
+	{3, 1023},
+	{3, 1024},
+}
 
 func main() {
-	log.Fatal("TODO")
+	for _, input := range inputs {
+		fmt.Printf("%v = %s\n", input.n, decompose(input.base, input.n).LaTeX())
+	}
 }
