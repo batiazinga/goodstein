ab2cc3e1e9cff38d8e68de349d0cc3fe21ae208b
Batiazinga <luc.nguyenthe@gmail.com>
2016-12-04 22:36:55 +0100

    Add unit tests for monomes

--- decomposition/decomposition_test.go
+++ decomposition/decomposition_test.go
@@ -1,6 +1,10 @@
 package decomposition
 
-import "fmt"
+import (
+	"fmt"
+	"math/big"
+	"testing"
+)
 
 func ExampleString() {
 	// decomposition of 0 (in any base)
@@ -48,4 +52,67 @@ func ExampleLaTeX() {
 	// 1
 	// 2 ^ {2 + 1} + 2
 	// 3 ^ {2} + 1
+}
+
+// unit tests for monomes
+
+type goldenMonome struct {
+	m             *monome
+	isZero, isOne bool
+	value         *big.Int
+}
+
+var goldenMonomes = []goldenMonome{
+	{
+		m: &monome{
+			coeff:    0,
+			base:     2,
+			exponent: nil,
+		},
+		isZero: true,
+		isOne:  false,
+		value:  big.NewInt(0),
+	},
+	{
+		m: &monome{
+			coeff:    1,
+			base:     2,
+			exponent: nil,
+		},
+		isZero: false,
+		isOne:  true,
+		value:  big.NewInt(1),
+	},
+	{
+		m: &monome{
+			coeff:    2,
+			base:     3,
+			exponent: nil,
+		},
+		isZero: false,
+		isOne:  false,
+		value:  big.NewInt(2),
+	},
+}
+
+func TestMonomeIsZero(t *testing.T) {
+	for _, g := range goldenMonomes {
+		if g.m.isZero() != g.isZero {
+			t.Errorf("wrong 'isZero' for %q", g.m)
+		}
+	}
+}
+func TestMonomeIsOne(t *testing.T) {
+	for _, g := range goldenMonomes {
+		if g.m.isOne() != g.isOne {
+			t.Errorf("wrong 'isOne' for %q", g.m)
+		}
+	}
+}
+func TestMonomeEval(t *testing.T) {
+	for _, g := range goldenMonomes {
+		if g.m.eval().Cmp(g.value) != 0 {
+			t.Errorf("wrong value %v for %q", g.m.eval(), g.m)
+		}
+	}
 }
