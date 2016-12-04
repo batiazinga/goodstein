c3f768908427ffff9c657a0b27ea4d6403059472
Batiazinga <luc.nguyenthe@gmail.com>
2016-12-04 22:45:00 +0100

    Fix typo in doc

--- decomposition/doc.go
+++ decomposition/doc.go
@@ -3,7 +3,7 @@ Package decomposition provides a hereditary base-b decomposition for the goodste
 
 The hereditary base-b decomposition of a positive integer n is
 
-    decompose(n) = \sum_{k=1}^{\lfloor\log_{b}(n)\rfloor} n_k \times b^{decompose(k)}
+    decompose(n) = \sum_{k=0}^{\lfloor\log_{b}(n)\rfloor} n_k \times b^{decompose(k)}
 
 where n_k is non negative and lower than b for all k.
 */
